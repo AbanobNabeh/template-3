e426868de0c45b0a0cb5419041b98e232daaaba8
abanobnabeh <abanobnabehrady@gmail.com>
2024-08-18 21:32:46 +0300

    progress

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ window.addEventListener("scroll", () => {
   let scrollTop = document.documentElement.scrollTop;
   el.style.width = `${(scrollTop / height) * 100}%`;
 });
+
+let ourSkills = document.querySelector(".our-skills");
+window.onscroll = function () {
+  let skillsOffesetTop = ourSkills.offsetTop;
+  let skillsOuterHeight = ourSkills.offsetHeight;
+  let windowHeight = this.innerHeight;
+  let windowScrollTop = this.pageYOffset;
+  if (windowScrollTop > skillsOffesetTop + skillsOuterHeight - windowHeight) {
+    let allSkills = document.querySelectorAll(
+      ".our-skills .container .skills .the-progress span"
+    );
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+    console.log(allSkills);
+  }
+};
